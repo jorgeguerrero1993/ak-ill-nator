b049c755dcf5dfce9caec601b786004e2b643ff7
jorgeguerrero1993 <jorgegn20@gmail.com>
2017-04-20 16:03:14 +0200

    tables finished

--- src/akillnator/jbdc/Dbmanager.java
+++ src/akillnator/jbdc/Dbmanager.java
@@ -118,7 +118,8 @@ public class Dbmanager {
 			"id INTEGER PRIMARY KEY AUTOINCREMENT,"+
 			"name INTEGER,"+
 			"text TEXT,"+
-			"aprox_duration TEXT)");
+			"aprox_duration TEXT )"+
+			"FOREIGN KEY (treatment_id) REFERENCES Treatment (id))");
 			//Treatment table
 			stmt.executeUpdate("CREATE TABLE Treatment("+
 			"id INTEGER PRIMARY KEY AUTOINCREMENT,"+
@@ -128,13 +129,37 @@ public class Dbmanager {
 			stmt.executeUpdate("CREATE TABLE Analysis("+
 			"id INTEGER PRIMARY KEY AUTOINCREMENT,"+
 			"name TEXT,"+
-			"FOREIGN KEY (illness_id) REFERENCES illness(id)");
+			"FOREIGN KEY (illness_id) REFERENCES Illness(id)");
 			//Patient-Previous events table
+			stmt.executeUpdate("CREATE TABLE PatientPrevEvents("+
+			"patId INTEGER REFERENCES Patient(id),"+
+			"prevId INTEGER REFERENCES PreviousEvents(id),"+
+			"PRIMARY KEY(patId,prevId))");
 			//Patient-symptons table
+			stmt.executeUpdate("CREATE TABLE PatientSymptons("+
+			"patId INTEGER REFERENCES Patient(id),"+
+			"sympId INTEGER REFERENCES Symptons(id))");
 			//Patient-drugs table
+			stmt.executeUpdate("CREATE TABLE PatientDrugs("+
+			"patId INTEGER REFERENCES Patient(id),"+
+			"drugsId INTEGER REFERENCES Drugs(id))");
 			//Previous events - symptons table
+			stmt.executeUpdate("CREATE TABLE PrevEventsSymptons("+
+			"prevId INTEGER REFERENCES PreviousEvents(id),"+
+			"sympId INTEGER REFERENCES Symptons(id)"+
+			"PRIMARY KEY(prevId,sympId))");
 			//Symptons - Illness table
+			stmt.executeUpdate("CREATE TABLE SymptonsIllness("+
+			"sympId INTEGER REFERENCES Symptons(id),"+
+			"illnessId INTEGER REFERENCES Illness(id),"+
+			"PRIMARY KEY(sympId,illnessId))");
 			//Drugs - Treatment table
+			stmt.executeUpdate("CREATE TABLE DrugsTreatment("+
+			"drugsId INTEGER REFERENCES Drugs(id),"+
+			"treatId INTEGER REFERENCES Treatment(id),"+
+			"quantity TEXT,"+
+			"Frequency TEXT,"+
+			"PRIMARY KEY (drugsId,treatId))");
 			//Patient - Ilness table
 			stmt.close();					
 		}
