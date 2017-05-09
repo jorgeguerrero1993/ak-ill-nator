b0aa730f6ed1d207fd6372bcca2271039507c17d
jorgeguerrero1993 <jorgegn20@gmail.com>
2017-05-09 16:34:21 +0200

    casi hecho jpa

--- src/akillnator/pojo/DrugsTreatment.java
+++ src/akillnator/pojo/DrugsTreatment.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
@@ -15,6 +17,10 @@ public class DrugsTreatment implements Serializable{
 	private Integer drugs_id;
 	@Id
 	private Integer treatment_id;
-	//@ManyToMany
-	
+	@ManyToOne
+	@PrimaryKeyJoinColumn(name="drugs_id", referencedColumnName="id")
+	private Drugs drugs;
+	@ManyToOne
+	@PrimaryKeyJoinColumn(name="treatment_id", referencedColumnName="id")
+	private Treatment treatment;
 }
--- src/akillnator/pojo/DrugsTreatmentId.java
+++ src/akillnator/pojo/DrugsTreatmentId.java
@@ -1,5 +1,23 @@
 package akillnator.pojo;
 
-public class DrugsTreatmentId {
+import java.io.Serializable;
 
+public class DrugsTreatmentId implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Integer drugs_id;
+	private Integer treatment_id;
+	
+	public int hashCode(){
+		return(int) (drugs_id + treatment_id);
+	}
+	
+	public boolean equals(Object obj){
+		if()
+		
+	}
 }
