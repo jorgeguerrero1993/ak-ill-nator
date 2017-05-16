0ed08f3695a49049ac555ba142032ca34497b62e
jorgeguerrero1993 <jorgegn20@gmail.com>
2017-05-16 16:27:57 +0200

    xml

---
+++ src/akillnator/xml/XMLmanager.java
@@ -0,0 +1,40 @@
+package akillnator.xml;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import akillnator.pojo.Patient;
+
+public class XMLmanager {
+	
+	File XMLFile=new File("./xmls/Patients.xml");
+	public void marshalling(Patient pat){
+		
+		try {
+			JAXBContext jaxbc=JAXBContext.newInstance(Patient.class);
+			Marshaller marshaller = jaxbc.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,Boolean.TRUE);
+			marshaller.marshal(pat, XMLFile);
+			marshaller.marshal(pat, System.out);
+			} catch (JAXBException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	public void unmarshakking(File XMLFile){
+		
+		try{
+			JAXBContext jaxbc=JAXBContext.newInstance(Patient.class);
+			Unmarshaller jaxbu=jaxbc.createUnmarshaller();
+			Patient pat=(Patient) jaxbu.unmarshal(XMLFile);
+			System.out.println(pat);
+		}catch(JAXBException ex){
+			ex.printStackTrace();
+		}
+	}
+}
